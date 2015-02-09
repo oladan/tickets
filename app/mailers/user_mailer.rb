5497e0ee9edf65a048d1d3f339d063662fe8cb2f
oladan <daniel@oladan.com>
2015-02-09 08:27:18 +0300

    Mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,15 +2,15 @@ class UserMailer < ActionMailer::Base
   default from: "from@example.com"
 
   def ticket_added_email(ticket)
-    # @ticket = ticket
-    # @ref = @ticket[:uniq_reference]
-    # mail(to: @ticket[:client_email], subject: 'Ticket added ' + @ref)
+    @ticket = ticket
+    @ref = @ticket[:uniq_reference]
+    mail(to: @ticket[:client_email], subject: 'Ticket added ' + @ref)
   end
 
   def ticket_replied_email(state_name, uniq_reference, reply_text, email)
-    # @ref = uniq_reference
-    # @status = state_name
-    # @reply_text = reply_text
-    # mail(to: email, subject: 'Ticket replied ' + @ref)
+    @ref = uniq_reference
+    @status = state_name
+    @reply_text = reply_text
+    mail(to: email, subject: 'Ticket replied ' + @ref)
   end
 end
